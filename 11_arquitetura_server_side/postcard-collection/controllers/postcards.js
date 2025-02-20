12e39a6658fb0e34852673ca06b68b38c036bb20
Vinicius Brustolin <vinnicius_brusto@hotmail.com>
2025-02-19 23:51:45 -0300

    conclusão aula refatoração separação em rotas e controllers

--- 11_arquitetura_server_side/postcard-collection/controllers/postcards.js
+++ 11_arquitetura_server_side/postcard-collection/controllers/postcards.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const postcardsPath = './postcards.json';
 
@@ -13,4 +14,90 @@ function listAll(res) {
         res.json(postcards);
     });
 }
+
+function listOne(req, res) {
+    const postId = req.params.id;
+
+    fs.readFile(postcardsPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read postcards data.' });
+        }
+
+        const postcards = JSON.parse(data);
+        const postcard = postcards.find((post) => post.id === postId);
+
+        if (!postcard) {
+            return res.status(404).json({ error: 'Postcard not found.' });
+        }
+
+        res.json(postcard);
+    });
+}
+
+function save(req, res) {
+    const { name, cidade, pais, descricao, imageUrl } = req.body;
+
+    const newPostcard = {
+        id: uuidv4(),
+        name,
+        cidade,
+        pais,
+        descricao,
+        imageUrl,
+    };
+
+    fs.readFile(postcardsPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read postcards data.' });
+        }
+
+        const postcards = JSON.parse(data);
+        postcards.push(newPostcard);
+
+        fs.writeFile(postcardsPath, JSON.stringify(postcards, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to add new postcard.' });
+            }
+
+            res.status(201).json(newPostcard);
+        });
+    });
+}
+
+function deleteOne(req, res) {
+
+    const { id } = req.params;
+
+    fs.readFile(postcardsPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read postcards data.' });
+        }
+
+        let postcards = JSON.parse(data);
+        const postcardIndex = postcards.findIndex((item) => item.id === id);
+
+        if (postcardIndex === -1) {
+            return res.status(404).json({ error: 'Postcard not found.' });
+        }
+
+        postcards = postcards.filter((item) => item.id !== id);
+
+        fs.writeFile(postcardsPath, JSON.stringify(postcards, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to delete postcard.' });
+            }
+
+            res.status(204).end();
+        });
+    });
+}
+
 exports.listAll = listAll;
+exports.listOne = listOne;
+exports.save = save;
+exports.deleteOne = deleteOne;
--- 11_arquitetura_server_side/postcard-collection/routes/postcards.js
+++ 11_arquitetura_server_side/postcard-collection/routes/postcards.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const { v4: uuidv4 } = require('uuid');
-const { listAll } = require('../controllers/postcards');
+const { listAll, listOne, save, deleteOne } = require('../controllers/postcards');
 
 router.get('/', (req, res) => {
     listAll(res);
-  });
+});
+
+router.get('/:id', (req, res) => {
+    listOne(req, res);
+});
+
+router.post('/', (req, res) => {
+    save(req, res);
+});
+
+router.delete('/:id', (req, res) => {
+    deleteOne(req, res);
+});
 
 module.exports = router;
 
