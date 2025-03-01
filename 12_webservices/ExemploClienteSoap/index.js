1e221d2bc7e28377dec1d2ab3f4157925b27377f
Vinicius Brustolin <vinnicius_brusto@hotmail.com>
2025-03-01 09:07:10 -0300

    aula webservices - soap 3
    mais funcoes do descritor wsdl

--- 12_webservices/ExemploClienteSoap/index.js
+++ 12_webservices/ExemploClienteSoap/index.js
@@ -2,19 +2,46 @@ const soap = require('soap');
 const url = 'http://www.dneonline.com/calculator.asmx?wsdl';
 
 soap.createClient(url, (err, client) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  const args = { intA: 2, intB: 3 };
-
-  client.Add(args, (err, result) => {
     if (err) {
-      console.error(err);
-      return;
+        console.error(err);
+        return;
     }
 
-    console.log(result);
-  });
+    const args = { intA: 2, intB: 3 };
+
+    client.Add(args, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log(result);
+    });
+
+    client.Subtract(args, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log(result);
+    });
+
+    client.Multiply(args, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log(result);
+    });
+
+    client.Divide({intA: 20, intB: 2}, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        console.log(result);
+    });
 });
