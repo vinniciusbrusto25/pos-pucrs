db71ddb1ebc8c66db40acc7b080e0812bc1ad840
Vinicius Brustolin <vinnicius_brusto@hotmail.com>
2025-08-30 10:27:34 -0300

    new feature

--- 22_projeto-desenvolvimento-aplicacao-web-pt-2/git-command/index.js
+++ 22_projeto-desenvolvimento-aplicacao-web-pt-2/git-command/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const port = process.env.PORT || 3001
 const app = express()
 
+app.get('/', (req, res) => {
+    console.log('New feature');
+})
+
 app.listen(port, () => {
     console.log(`start listening ${port}`)
 })
