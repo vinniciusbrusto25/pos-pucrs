aeef06bda93a62efb3ff7257e823ac41c8d44f71
Vinicius Brustolin <vinnicius_brusto@hotmail.com>
2025-08-30 11:02:32 -0300

    ajuste main

--- 22_projeto-desenvolvimento-aplicacao-web-pt-2/git-command/index.js
+++ 22_projeto-desenvolvimento-aplicacao-web-pt-2/git-command/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     console.log('New feature');
 })
 
+app.get('/feature', (req, res) => {
+    console.log('feito na main');
+})
+
 app.listen(port, () => {
     console.log(`start listening ${port}`)
 })
